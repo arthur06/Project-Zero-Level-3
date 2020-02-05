c617ed5886efe59d0b1bdbf1e3c1ac10a438c9b5
arthur <arthur.fokker@coderclass.nl>
2020-02-05 09:20:57 +0100

    verbeterd

--- index.js
+++
@@ -1,7 +0,0 @@
-//var settings_class = document.getElementsByClassName("setting");
-//var settings = ["setting1", "setting2", "setting3", "setting4"];
-//for (var j = 0; j < settings_class.length; j++) { settings_class[j].addEventListener("click", change(settings_class[j]));}
-//function change(J) {
-//    var x = J.texContent;
-//    console.log(x);
-//}
